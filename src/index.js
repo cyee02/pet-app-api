a824b8d94e7345a3bff37b10955b87bbd59ce016
cyee02 <chiazy94@gmail.com>
2021-08-30 02:36:26 +0800

    remove region

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-const { ApolloServer } = require('apollo-server-lambda');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./graphql/schema.js')
 const  { verifyToken } = require('./utils/authService')
 const getUser = require('./aws/getUser')
 
+const app = express();
+const PORT = process.env.SERVER_PORT || 4000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({ event }) => {
-    const authorization = event ? event.headers.Authorization : null
+  context: async ({ req }) => {
+    const authorization = req ? req.headers.authorization : null
     if (authorization) {
       const accessToken = verifyToken(authorization)
       const user = await getUser(accessToken.userId)
@@ -16,6 +20,35 @@ const server = new ApolloServer({
       }
     }
   },
+  playground: true
 })
 
-exports.handler = server.createHandler()
+server.applyMiddleware({
+  app
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+})
+
+// const { ApolloServer } = require('apollo-server-lambda');
+// const { typeDefs, resolvers } = require('./graphql/schema.js')
+// const  { verifyToken } = require('./utils/authService')
+// const getUser = require('./aws/getUser')
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: async ({ event }) => {
+//     const authorization = event ? event.headers.Authorization : null
+//     if (authorization) {
+//       const accessToken = verifyToken(authorization)
+//       const user = await getUser(accessToken.userId)
+//       return {
+//         user
+//       }
+//     }
+//   },
+// })
+
+// exports.handler = server.createHandler()
